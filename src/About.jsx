fbc162687bd142e5a81c02883c87a7229b333b7e
sarang820 <sarangagarwal101@gmail.com>
2021-01-12 15:41:01 +0530

    comiit1

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,67 @@
+
 import Axios from "axios";
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+
 
 const About=()=>{
+    const url="https://api.spacexdata.com/v4/company";
+    const [data, setData] =  useState("");
+
+    const sendGetRequest = async () => {
+        const resp = await Axios.get(url);
+        setData(resp.data);
+};
+    if(data===""){
+    sendGetRequest();
+    }
+    console.log(data.links);
     return(
        <>
-       <h1>This is about section</h1>
+       
+       <div className="about-info">
+           <div>
+               <h1>Headquaters</h1>
+               <ul>
+                    <li>Address: Rocket Road</li>
+                    <li>City: Hawthrone</li>
+                    <li>State: California</li>
+               </ul>
+               <hr/>
+           </div>
+           
+           <div>
+               <h1>Links</h1>
+               <ul>
+                    <li><a href="https://www.spacex.com/">Website </a> </li>
+                    <li><a href="https://www.flickr.com/photos/spacex/">flickr</a></li>
+                    <li><a href="https://twitter.com/SpaceX">Twitter</a></li>
+                    <li><a href="https://twitter.com/elonmusk">Elon_Twitter </a></li>
+               </ul>
+               <hr/>
+           </div>
+
+           <div>
+               <h1>Details</h1>
+               <ul>
+                    <li>Name: {data.name}</li>
+                    <li>Founder: {data.founder}</li>
+                    <li>Founded: {data.founded}</li>
+                    <li>Employees: {data.employees}</li>
+                    <li>Vehicles: {data.vehicles}</li>
+                    <li>Launch_sites: {data.launch_sites}</li>
+                    <li>Test_sites: {data.test_sites}</li>
+                    <li>CEO: {data.ceo}</li>
+                    <li>CTO: {data.cto}</li>
+                    <li>COO: {data.coo}</li>
+                    <li>CTO_propulsions: {data.cto_propulsion}</li>
+                    <li>Valuation: {data.valuation}</li>
+                    <li>Summary: {data.summary}</li>
+                    <li>Id: {data.id}</li>
+               </ul>
+               <hr/>
+           </div>
+       </div>
        </> 
     )
 }
--- src/Past_Launches.jsx
+++ src/Past_Launches.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const Past_Launches=()=>{
     return(
         <>
-        <h1>This is Past_Launches page</h1>
+        <h1 className="text-center my-5">This is Past_Launches page</h1>
+        <div className="container">
+  <div className="row">
+    <div className="col-sm">
+      <div class="card" >
+  <img src="https://picsum.photos/200/301" class="card-img-top" alt="..." height="200px" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div className="col-sm">
+      <div class="card" >
+  <img src="https://picsum.photos/201/300" class="card-img-top" alt="..." height="200px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+    <div className="col-sm">
+      <div class="card" >
+  <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." height="200px"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
         </>
     )
 }
